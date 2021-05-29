3edbafc0012d80718f77f65a36974b9a56ab98b7
donggi <ldg6153@gmail.com>
2021-05-29 22:44:57 +0900

    Add find charge intent

--- src/main/java/carrot/chatbot/proto/dialogflow/CarrotChatbot.java
+++ src/main/java/carrot/chatbot/proto/dialogflow/CarrotChatbot.java
@@ -81,6 +81,27 @@ public class CarrotChatbot {
             payload.putFields("o2o", Value.newBuilder().setStructValue(o2o).build());
 
         }
+
+        else if (intentName.equals("find charge")) {
+            //Simple Response
+            displayText = "찾으시는 물품에 대해 평균 가격을 알려드립니다";
+            tts = "찾으시는 물품에 대해 평균 가격을 알려드립니다";
+            simpleResponse = o2oTemplate.makeSimpleResponse(displayText, tts);
+
+            //Basic Card
+            image = o2oTemplate.makeImage("https://actions.o2o.kr/content/new/Skylife-ServiceCenter/Ko/aipersonKo.gif", "Fallback 이미지");
+            description = "찾으시는 물품에 대해 평균 가격을 알려드립니다";
+            basicCard = o2oTemplate.makeBasicCard(null, null, description, image, null);
+
+            //Suggestions
+            suggestions = o2oTemplate.makeSuggestionChips("화면이 이상해요", "인터넷 연결 방법");
+
+            o2o.putFields("simpleResponse", Value.newBuilder().setStructValue(simpleResponse).build());
+            o2o.putFields("basicCard", Value.newBuilder().setStructValue(basicCard).build());
+            o2o.putFields("suggestions", Value.newBuilder().setListValue(suggestions).build());
+
+            payload.putFields("o2o", Value.newBuilder().setStructValue(o2o).build());
+        }
         try {
             jsonResponse = JsonFormat.printer().print(
                     WebhookResponse.newBuilder().setPayload(payload).build()
@@ -88,6 +109,8 @@ public class CarrotChatbot {
         } catch (InvalidProtocolBufferException e) {
             e.printStackTrace();
         }
+
+
 
         System.out.println("jsonResponse >>");
         System.out.println(jsonResponse);
